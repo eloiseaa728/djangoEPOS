66a8c0b1569b96878088c33bb936020fb0636cfe
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-01-20 19:31:43 +0000

    changed to postgres

--- djangoEPOS/settings.py
+++ djangoEPOS/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,13 +78,17 @@ WSGI_APPLICATION = 'djangoEPOS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(getenv('dbName')),
+        'USER': str(getenv('dbUser')),
+        'PASSWORD': str(getenv('dbPass')),
+        'HOST': str(getenv('dbHost')),
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
