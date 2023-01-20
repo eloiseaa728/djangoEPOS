9e170ce1c29779e2b7a1ae9516491aea1b3dea42
Els Allaway <66450509+eloiseaa728@users.noreply.github.com>
2023-01-20 17:02:46 +0000

    Remove secret key from git (has been changed lol)

--- djangoEPOS/settings.py
+++ djangoEPOS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hr_(y^hp@a6r2#$@dyz%lmi^qkijh(!q0zoi4m39ct9t+evb_s'
+SECRET_KEY = str(getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
